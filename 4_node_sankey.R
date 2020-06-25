e05fd200795b8075053b78f9a85cfd3c156807a2
mlr245 <mruelle@clarku.edu>
2020-06-25 17:26:51 -0400

    Added significance!

--- 4_node_sankey.R
+++ 4_node_sankey.R
@@ -9,7 +9,7 @@ library(readxl)
 library(readr)
 
 #Read in each sheet individually which creates a Tibble for each.
-#comment out either the below survey or experiement to use the other
+#comment out either the below survey or experiment to use the other
 #-------------------------------------------------------------------------
 
 #Experiment paths
@@ -25,14 +25,18 @@ library(readr)
 
 #-------------------------------------------------------------------------
 #Survey paths
+# Set working directory for Ryan
+#setwd("C:/Users/ryankopper/Clark University/Morgan Ruelle - Ryan Kopper Research/Push Pull Systematic Review/")
+# Set working directory for Morgan
+setwd("C:/Users/Morgan/OneDrive - Clark University/Ryan Kopper Research/Push Pull Systematic Review/")
 
-Relationships <- read_excel("/Users/ryankopper/Clark University/Morgan Ruelle - Ryan Kopper Research/Push Pull Systematic Review/Survey_4dummy_sankey.xlsx", sheet = "Relationships")
+Relationships <- read_excel("Survey_4dummy_sankey.xlsx", sheet = "Relationships")
 #Relationships <- data.frame(lapply(Relationships, as.factor))
 
-Independent_recode <- read_excel("/Users/ryankopper/Clark University/Morgan Ruelle - Ryan Kopper Research/Push Pull Systematic Review/Survey_4dummy_sankey.xlsx", sheet = "Independent recode")
+Independent_recode <- read_excel("Survey_4dummy_sankey.xlsx", sheet = "Independent recode")
 #Independent_recode <- data.frame(lapply(Independent_recode, as.factor))
 
-Dependent_recode <- read_excel("/Users/ryankopper/Clark University/Morgan Ruelle - Ryan Kopper Research/Push Pull Systematic Review/Survey_4dummy_sankey.xlsx", sheet = "Dependent recode")
+Dependent_recode <- read_excel("Survey_4dummy_sankey.xlsx", sheet = "Dependent recode")
 #Dependent_recode <- data.frame(lapply(Dependent_recode, as.factor))
 
 
@@ -43,29 +47,43 @@ distinct_rel <- left_join( Relationships, Independent_recode,
   left_join(Dependent_recode, by = "Dependent_variable") %>%
   distinct(Title, Independent_variable_recode,
            Dependent_variable_recode, Independent_broad_category,
-           Dependent_broad_category )
+           Dependent_broad_category)
 
-
-BIV_2_BDV <- distinct_rel %>% 
-  select(Title, Independent_broad_category, Dependent_broad_category) %>%
-  group_by(Independent_broad_category, Dependent_broad_category) %>%
+# Adding significance the distinct relations
+distinct_rel_sig <- Relationships %>% 
+  left_join(Independent_recode, by = "Independent_variable") %>%
+  left_join(Dependent_recode, by = "Dependent_variable") %>%
+  select(Independent_variable_recode, Dependent_variable_recode, Relationship) %>% 
+  group_by(Independent_variable_recode, Dependent_variable_recode) %>%
+  summarise(sig = sum(Relationship=="significant"),
+            not_sig = sum(Relationship=="not significant"),
+            mixed = sum(Relationship=="mixed"),
+            total = n()) %>%  
+  mutate(significance = if_else(sig==total,"sig",
+                                if_else(not_sig==total,"not_sig","mixed"))) %>%
+  full_join(distinct_rel) %>% 
+  select(Title,Independent_variable_recode,Independent_broad_category,
+         Dependent_variable_recode,Dependent_broad_category,significance)
+  
+BIV_2_BDV <- distinct_rel_sig %>%
+  select(Title, Independent_broad_category, Dependent_broad_category,significance) %>%
+  group_by(Independent_broad_category, Dependent_broad_category,significance) %>%
   summarise(count = n()) %>% rename(target = Dependent_broad_category,
-                                    source = Independent_broad_category)
+                                    source = Independent_broad_category,
+                                    link_group = significance)
 
 IV_2_BIV <- distinct_rel %>%
   group_by(Independent_variable_recode, Independent_broad_category) %>%
   summarise(count = n()) %>%
-  rename(source = Independent_variable_recode, target = Independent_broad_category)
+  rename(source = Independent_variable_recode, target = Independent_broad_category) %>% 
+  mutate(link_group="subset")
 
 
 DV_2_BDV <- distinct_rel %>%
   group_by(Dependent_variable_recode, Dependent_broad_category) %>%
   summarise(count = n()) %>%
-  rename(target = Dependent_variable_recode, source = Dependent_broad_category)
-
-
-
-
+  rename(target = Dependent_variable_recode, source = Dependent_broad_category) %>% 
+  mutate(link_group="subset")
 
 joined_long <- bind_rows(IV_2_BIV , DV_2_BDV, BIV_2_BDV)
 
@@ -80,25 +98,32 @@ BIV <- unique(Independent_recode$Independent_broad_category)
 BDV <- unique(Dependent_recode$Dependent_broad_category)
 total_var <- length(IV) + length(DV) + length(BIV) + length(BDV) - 1
 
-node <- data.frame(key = 0:total_var,
+nodes <- data.frame(key = 0:total_var,
                    variable = as.factor(c(as.character(IV),as.character(DV),
-                                          as.character(BIV), as.character(BDV))))
+                                          as.character(BIV), as.character(BDV)))) %>% 
+  mutate(node_group="all")
 
 
 #create links dataframe and rename 
-links <- joined_long %>% inner_join(node,  by = c("source" = "variable")) %>% rename(source_key = key) %>% 
-  inner_join(node,  by = c("target" = "variable")) %>% rename(target_key = key)
+links <- joined_long %>%
+  inner_join(nodes,  by = c("source" = "variable"))%>%
+  rename(source_key = key) %>% 
+  inner_join(nodes,  by = c("target" = "variable")) %>%
+  rename(target_key = key) %>% 
+  select(source,target,source_key,target_key,count,link_group)
 
 
-my_color <- 'd3.scaleOrdinal() .domain(["source","target"]) .range(["red", "green"])'
+my_color <- 'd3.scaleOrdinal() .domain(["all","mixed","not_sig","sig","subset"]) .range(["tan", "yellow" , "red", "darkgreen","lightgrey"])'
 
 sankeyD3::sankeyNetwork(Links = links,
-                        Nodes = node,
+                        Nodes = nodes,
                         Source = 'source_key', 
                         Target = 'target_key',
                         Value = 'count',
-                        NodeID = "variable", 
-                        #colourScale = my_color,
+                        NodeID = "variable",
+                        LinkGroup = "link_group",
+                        NodeGroup = "node_group",
+                        colourScale = my_color,
                         fontSize = 12,
                         fontFamily = "sans-serif",
                         orderByPath = F,
